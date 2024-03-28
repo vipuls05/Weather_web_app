105972ca6f5e0865e16eb14c9b46ff723dd324ba
Vipul  Kumar Singh <162988342+vipuls05@users.noreply.github.com>
2024-03-28 11:49:23 +0530

    Add files via upload

---
+++ Weather/Ultimate-Wather-App-main/README.md
@@ -0,0 +1,24 @@
+# Weather App
+
+This is a weather application that allows users to search for weather data for different cities. Users can enter a city name in the search box and get the current weather information.
+
+## Features
+
+- Search for weather data for cities.
+- Display current weather information, including temperature, humidity, wind speed, and description.
+- Display the forecast for the next 5 days.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Axios library for making HTTP requests
+- SheCodes Weather API for fetching weather data
+
+## Usage
+
+1. Enter a city name in the search box.
+2. Press Enter or click the search button.
+3. The current weather information for the entered city will be displayed.
+4. Scroll down to view the forecast for the next 5 days.
---
+++ Weather/Ultimate-Wather-App-main/src/index.js
@@ -0,0 +1,106 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+   let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let d = new Date();
+  let day = weekday[d.getDay()];
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
+ 
+
+  cityElement.innerHTML = response.data.city;
+  document.getElementById("weekday").innerHTML = day;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  getForecast(response.data.city);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 6) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div>
+         <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+        </div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-max">
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
+          </div>
+          <div class="weather-forecast-min">${Math.round(day.temperature.minimum)}°</div>
+        </div>
+      </div>
+    `;
+   }
+    });
+
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Düsseldorf");
